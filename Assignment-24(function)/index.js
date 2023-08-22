7d53767bddc621e4257802c6102d315b47cf6423
yogitashete2412 <yogitashete24@gmail.com>
2023-08-22 08:56:13 +0530

    ref: example added

---
+++ Assignment-24(function)/index.js
@@ -0,0 +1,26 @@
+// function sayHello(name)
+// {
+//     console.log(`Hello ${name}`);
+// }
+// sayHello();
+// sayHello();
+
+// function calSquareArea(side)
+// {
+//     const area = side * side
+//     console.log(`Area : ${area}`);
+// }
+// calSquareArea(10);
+
+// function greet(name="User", day="next class")
+// {
+//     console.log(`Hello ${name}. Your interview is scheduled on ${day}.`);
+// }
+// greet();
+
+function greet(name, day="next class")
+{
+    console.log(`Hello ${name}. Your interview is scheduled on ${day}.`);
+}
+greet('Suraj');
+
