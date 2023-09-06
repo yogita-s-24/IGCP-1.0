d7bcb26db0851a72b1f1a101705777bad0598b40
yogitashete2412 <yogitashete24@gmail.com>
2023-09-06 17:15:23 +0530

    doc : assignment added

---
+++ Assignment-33(programs)/index.js
@@ -0,0 +1,21 @@
+//Program to print even numbers from 1 to 20.
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+//Program to print odd numbers from 1 to 20.
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 == 1) {
+//         console.log(i);
+//     }
+// }
+
+//Program to print numbers from 1 to 10 in reverse order.
+
+for (let i = 10; i>=1; i--) {
+        console.log(i);
+}
