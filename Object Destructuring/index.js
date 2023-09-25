5d6c48f289cdcdebc0b8d732135789bf94a270c7
yogitashete2412 <yogitashete24@gmail.com>
2023-09-25 23:07:06 +0530

    formated all code

---
+++ Object Destructuring/index.js
@@ -0,0 +1,15 @@
+// const user={
+//     name:"Yogita",
+//     age:"22",
+//     degree:"Mcs"
+// }
+// console.log(user.name);
+// console.log(user.age);
+// console.log(user.degree);
+
+//Oject destructuring
+const {name,age,degree} = user;
+
+console.log(name);
+console.log(age);
+console.log(degree);
